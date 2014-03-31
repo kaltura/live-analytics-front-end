5833624495dbb73b54bf10cea200dfb3d89da4f7
Atar Shadmi <atar.shadmi@kaltura.com>
2014-03-31 16:39:57 +0300

    add player

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,8 +4,32 @@
 
 var analyticsDirectives = angular.module('analyticsDirectives', []);
 
-analyticsDirectives.directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+
+analyticsDirectives.directive('kplayer', function() {
+	return {
+		
+		restrict : 'E',
+		scope: {
+			entryid: '@',
+			uiconf: '=',	
+			pid: '='	
+		},
+		//replace : true,
+		template: '<div id="kplayer" style="width:100%; height:100%;"></div>',
+		link: function($scope, element, attrs) {
+			console.log('s');
+			kWidget.embed({
+            	"targetId": "kplayer", 
+            	"wid": "_" +$scope.pid, 
+              	"uiconf_id": $scope.uiconf, 
+              	"flashvars": { 
+              		"streamerType": "auto" 
+              	}, 
+              	"cache_st": 1395933525, 
+              	"entry_id": $scope.entryid 
+              });
+			
+		}
+	};
+});
+
