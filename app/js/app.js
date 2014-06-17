bb805b4a54f88f221be4aef2666d902db3a4b95b
Atar Shadmi <atar.shadmi@kaltura.com>
2014-06-17 16:22:52 +0300

    PLAT-1370 hardcoded en_US translations

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 var liveAnalytics = angular.module('liveAnalytics', [
 	'ngRoute',
+	'pascalprecht.translate',
 	'analyticsFilters',
 	'analyticsServices',
 	'analyticsDirectives',
@@ -11,7 +12,8 @@ var liveAnalytics = angular.module('liveAnalytics', [
 	'analyticsFilters'
 ]);
 
-liveAnalytics.config(['$routeProvider', function($routeProvider) {
+liveAnalytics.config(['$routeProvider', '$translateProvider', function($routeProvider, $translateProvider) {
+	// routes
 	$routeProvider.when('/dashboard', {
 		templateUrl: 'partials/dashboard.html', 
 		controller: 'DashboardCtrl'
@@ -24,6 +26,57 @@ liveAnalytics.config(['$routeProvider', function($routeProvider) {
 		template: '<div>Missing KS</div>' 
 	});
 	$routeProvider.otherwise({redirectTo: '/dashboard'});
+	
+	// translates
+	$translateProvider.translations('en_US', {
+		// aggregates:
+		'audience': 'Audience',
+		'seconds_viewed': 'Minutes Viewed', // this is not a mistake, text should read minutes
+		'buffertime': 'Buffering Time',
+		'bitrate': 'Average Bitrate',
+		
+		// dashboard texts:
+		'dashboard' : {
+			'Live_Content_36': 'Live Content (in past 36 hours)',
+			'Show_All_Entries': 'Show All Entries',
+			'Show_Kaltura': 'Show Kaltura Live Now Only',
+			'Audience': 'Audience',
+			'Peak_Audience': 'Peak Audience',
+			'Minutes_Viewed': 'Minutes Viewed',
+			'Buffer_Time': 'Buffering Time',
+			'Avg_Bitrate': 'Average Bitrate',
+			'Investigate': 'Investigate'
+		},
+		
+		// entry texts:
+		'entry' : {
+			'Live_Content': 'Live Content',
+			'Investigate': 'Investigate',
+			'Audience': 'Audience',
+			'Location': 'Location',
+			'Top_referals': 'Top referals',
+			'Domain': 'Domain',
+			'Visits': 'Visits',
+			'of_total': '% of total'
+		},
+		
+		// index texts:
+		'main' : {
+			// tab names should match names in KMC
+			'Content_Reports': 'Content Reports',
+			'Community_Reports': 'Users & Community Reports',
+			'Storage_Reports': 'Bandwidth & Storage Reports',
+			'System_Reports': 'System Reports',
+			'Live_Reports': 'Live Reports'
+		}
+	});
+		 
+	$translateProvider.translations('de_DE', {
+		'Show_All_Entries': 'Show_All_Entries in german',
+		'FOO': 'Dies ist ein Paragraph'
+	});
+		 
+	$translateProvider.preferredLanguage('en_US');
 }]);
 
 function navigateToFlashAnalytics(subtabName) {
